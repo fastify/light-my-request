36186fc0db4ffe3d1d730779811f73e999dd7ab1
delvedor <tommydelved@gmail.com>
2017-09-28 00:42:57 +0200

    Updated library to support node >= 4

--- examples/http.js
+++ examples/http.js
@@ -1,29 +1,23 @@
-'use strict';
+'use strict'
 
 // Load modules
 
-const Shot = require('..');
-
+const Shot = require('..')
 
 // Declare internals
 
-const internals = {};
-
+const internals = {}
 
 internals.main = function () {
+  const dispatch = function (req, res) {
+    const reply = 'Hello World'
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    res.end(reply)
+  }
 
-    const dispatch = function (req, res) {
+  Shot.inject(dispatch, { method: 'get', url: '/' }, (res) => {
+    console.log(res.payload)
+  })
+}
 
-        const reply = 'Hello World';
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end(reply);
-    };
-
-    Shot.inject(dispatch, { method: 'get', url: '/' }, (res) => {
-
-        console.log(res.payload);
-    });
-};
-
-
-internals.main();
+internals.main()
