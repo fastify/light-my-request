9e496480dd44fff20b7edbc1ccb5d1a9cc907a7b
delvedor <tommydelved@gmail.com>
2017-09-28 14:57:25 +0200

    Promises and async await support

--- index.js
+++ index.js
@@ -62,10 +62,19 @@ function inject (dispatchFunc, options, callback) {
     }
   }
 
-  const req = new Request(options)
-  const res = new Response(req, callback)
+  if (typeof callback === 'function') {
+    const req = new Request(options)
+    const res = new Response(req, callback)
 
-  return req.prepare(() => dispatchFunc(req, res))
+    return req.prepare(() => dispatchFunc(req, res))
+  } else {
+    return new Promise((resolve) => {
+      const req = new Request(options)
+      const res = new Response(req, resolve)
+
+      req.prepare(() => dispatchFunc(req, res))
+    })
+  }
 }
 
 function isInjection (obj) {
