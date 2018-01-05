0ea481aa66d23600b9f483923cb624fe53d0ae3f
delvedor <tommydelved@gmail.com>
2018-01-05 20:25:43 +0100

    Updated test

--- test/async-await.js
+++ test/async-await.js
@@ -1,14 +1,34 @@
 'use strict'
 
-async function asyncAwaitTest (t, inject) {
-  t.plan(1)
-  const dispatch = function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.end('hello')
-  }
+function asyncAwaitTest (t, inject) {
+  t.plan(2)
 
-  const res = await inject(dispatch, { method: 'get', url: 'http://example.com:8080/hello' })
-  t.equal(res.payload, 'hello')
+  t.test('basic async await', async t => {
+    const dispatch = function (req, res) {
+      res.writeHead(200, { 'Content-Type': 'text/plain' })
+      res.end('hello')
+    }
+
+    try {
+      const res = await inject(dispatch, { method: 'get', url: 'http://example.com:8080/hello' })
+      t.equal(res.payload, 'hello')
+    } catch (err) {
+      t.fail(err)
+    }
+  })
+
+  t.test('basic async await (errored)', async t => {
+    const dispatch = function (req, res) {
+      res.connection.destroy(new Error('kaboom'))
+    }
+
+    try {
+      await inject(dispatch, { method: 'get', url: 'http://example.com:8080/hello' })
+      t.fail('should throw')
+    } catch (err) {
+      t.ok(err)
+    }
+  })
 }
 
 module.exports = asyncAwaitTest
