f2a897fdf1e482b569496997b6cbd43c071fad6b
Simone Busoli <simone.busoli@gmail.com>
2022-08-22 10:54:58 +0200

    fix: request has aborted flag set to false (#218)

---
+++ test/request.test.js
@@ -0,0 +1,14 @@
+const { test } = require('tap')
+
+const Request = require('../lib/request')
+
+test('aborted property should be false', async (t) => {
+  const mockReq = {
+    url: 'http://localhost',
+    method: 'GET',
+    headers: {}
+  }
+  const req = new Request(mockReq)
+
+  t.same(req.aborted, false)
+})
